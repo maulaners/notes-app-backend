0a4ef7dc27b7b5180fa5db34fc2da0c356106029
maulana d.p <88878659+maulaners@users.noreply.github.com>
2023-07-31 20:31:02 +0700

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,11 +1,17 @@
 const Hapi = require("@hapi/hapi");
 const routes = require('./routes');
 
-/*	server.route(routes);
+	const init = async () => {
+	const server = Hapi.server({
+		port: 5000,
+		host: 'localhost',
+	});
+
+server.route(routes);
 	
 	await server.start();
 	console.log(`server berjalan pada ${server.info.uri}`);
-};*/
+};
 const server = Hapi.server({
 	port: 5000,
 	host: proccess.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
