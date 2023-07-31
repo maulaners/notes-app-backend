eba52124a2a180d2ffffc4bd1e8a56fb87346061
maulana d.p <88878659+maulaners@users.noreply.github.com>
2023-07-31 21:10:20 +0700

    Update server.js

--- src/server.js
+++ src/server.js
@@ -5,6 +5,11 @@ const routes = require('./routes');
 	const server = Hapi.server({
 		port: 5000,
 		host: 'localhost',
+		routes: {
+			cors: {
+				origin: ['*'],
+			},
+		},
 	});
 
 server.route(routes);
